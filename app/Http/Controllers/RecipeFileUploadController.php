2411fa3b87a3c0b0d9e293df02a5cafedb442226
LB16B <ogawa.yui2000@gmail.com>
2023-08-10 07:37:19 +0900

    cropper

--- app/Http/Controllers/RecipeFileUploadController.php
+++ app/Http/Controllers/RecipeFileUploadController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
 
 class RecipeFileUploadController extends Controller
 {
@@ -14,16 +16,30 @@ class RecipeFileUploadController extends Controller
             // ファイルがアップロードされた場合
             $file = $request->file('file');
 
+            // トリミング情報を取得
+            $x = $request->input('x');
+            $y = $request->input('y');
+            $height = $request->input('height');
+            $with = $request->input('with$with');
+
+            // 画像加工
+            $trimmingFile = Image::make($file);
+            $file->crop($width, $height, $x, $y);
+
+            
             // ファイルを保存するディレクトリを指定（任意のディレクトリに変更する）
             $uploadPath = public_path('recipe_images');
-
+            
             $jstDateTime = date('YmdHi', strtotime('+ 9 hours', time()));
             $fileName = $jstDateTime . '_' . $file->getClientOriginalName();
-            $file->move($uploadPath, $fileName);
+            // $file->save($uploadPath, $fileName);
+            
+            $file->save($uploadPath . '/' . $fileName);
 
             return response()->json(['message' => 'File uploaded successfully.', 'filename' => $fileName]);
         } else {
             return response()->json(['message' => 'No file uploaded.'], 400);
         }
+        
     }
 }
