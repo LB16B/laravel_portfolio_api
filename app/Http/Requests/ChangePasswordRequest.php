d7bbeb5685497d9a7968be5d83957267ddf32549
LB16B <ogawa.yui2000@gmail.com>
2023-10-10 22:37:58 +0900

    change password

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -14,9 +14,9 @@ class ChangePasswordRequest extends FormRequest
     public function rules()
     {
         return [
-            'current_password' => 'required|password', 
+            'current_password' => 'required', 
             'new_password' => 'required|string|min:8|different:current_password', 
-            'new_confirm_password' => 'required|string|same:new_password', 
+            'new_password_confirmation' => 'required|string|same:new_password', 
         ];
     }
 }
